43ea5f7e8a10051c5041b6f86d40baccfc5d3a6a
YangShi2013 <shiyang2012@u.northwestern.edu>
2013-07-02 17:05:27 +0800

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IBM2013::Application.config.secret_key_base = '52ff90ec277053c122f200eab3a58297bae1686e631c71f54b95af8f565596ef0da710ed60354feac61fed690a724406afec0579896c0d0a096fd0bb2cd7c1fd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IBM2013::Application.config.session_store :cookie_store, key: '_IBM2013_session'
